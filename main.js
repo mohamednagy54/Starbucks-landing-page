ed4441432a6e04d0efe8fc67c8f24324d5d34b19
mohamednagy54 <92611426+mohamednagy54@users.noreply.github.com>
2022-09-28 11:11:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+let starbucks = document.querySelector('.starbucks')
+let circle = document.querySelector('.circle')
+
+
+function imgSlider(img){
+    starbucks.src = img
+}
+
+
+function changeCircleColor(color){
+    circle.style.background = color;
+
+}
